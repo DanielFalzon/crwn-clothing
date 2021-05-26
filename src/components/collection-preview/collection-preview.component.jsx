ce659780aa9991a7498d8b3234fcfd46f6e84537
Daniel Falzon <daniel.falzon@raketech.com>
2021-05-26 15:53:02 +0200

    Added collection items in preview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import CollectionItem from '../collection-item/collection-item.component';
+
 import './collection-preview.styles.scss';
 
 //Performance concern since all the function calls will always get called with every re render of the component 
@@ -10,8 +12,8 @@ const CollectionPreview = ({title, items}) => (
             {
                 items
                     .filter((item, idx) => idx < 4)
-                    .map((item) => (
-                        <div key={item.id} >{item.name}</div>
+                    .map(({id, ...otherItemProps}) => (
+                        <CollectionItem key={id} { ...otherItemProps } />
                     ))
             }
         </div>
