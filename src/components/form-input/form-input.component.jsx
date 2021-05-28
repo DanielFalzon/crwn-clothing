e693d0f75212919eaf068e83364ff2cb165a900e
Daniel Falzon <daniel.falzon@raketech.com>
2021-05-28 07:51:10 +0200

    Added reusable sign in components

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -4,13 +4,15 @@ import './form-input.styles.scss';
 
 const FormInput = ({ handleChange, label, ...otherProps}) => (
     <div className="group">
-        <input className='form-input' onChange={handlechange} {...otherProps} />
+        <input className='form-input' onChange={handleChange} {...otherProps} />
         {
             label ? 
-            (<label className={`${otherProps.value.length}` ? 'strink' : ''} form-input-label>
-
+            (<label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
+                {label}
             </label>)
             : null
         }
     </div>
 )
+
+export default FormInput;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -31,12 +34,24 @@ class SignIn extends React.Component{
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input name='email' type='email' value={this.state.email} required onChange={this.handleChange} />
-                    <label>Email</label>
-                    <input name='password' type='password' value={this.state.password} required onChange={this.handleChange} />
-                    <label>Password</label>
+                    <FormInput 
+                        name='email' 
+                        type='email' 
+                        value={this.state.email} 
+                        handleChange={this.handleChange}
+                        label="email" 
+                        required
+                    />
+                    <FormInput
+                        name='password'
+                        type='password' 
+                        value={this.state.password}
+                        handleChange={this.handleChange}
+                        label="password"
+                        required 
+                    />
 
-                    <input type='submit' value='Submit Form' />
+                    <CustomButton type='submit'> Sign In </CustomButton>
                 </form>
             </div>
         )
