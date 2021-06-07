ae19e5e37e50ec7fe06b22e2370cfd38ad434494
Daniel Falzon <daniel.falzon@raketech.com>
2021-06-07 21:17:43 +0200

    Implement firebase authentication with email and gmail

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component  {
   constructor(){
@@ -21,8 +21,23 @@ class App extends React.Component  {
   unsubscribedFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribedFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
+    this.unsubscribedFromAuth = auth.onAuthStateChanged( async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          });
+          console.log( this.state );
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
+
     });
   }
 
