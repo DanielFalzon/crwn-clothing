f68cf1d5a71211bbf0f9b7c27358df02538e7ced
Daniel Falzon <daniel.falzon@raketech.com>
2021-06-13 12:22:46 +0200

    Added cart icon and dropdown and redux hidden state

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 
 import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
 
 //Store holding all of the data in the entire site.
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 })
