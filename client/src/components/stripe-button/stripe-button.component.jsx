32b5ce40d75013dfa5d29bd4e30da55c13ee3dd5
Daniel Falzon <daniel.falzon@raketech.com>
2021-08-06 21:34:35 +0200

    Seperating fe into clients and backend server

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51J34YOAZ1WBe7cFW0aBZWlxiqEiGLDwn8MBGb8grKNr0OoIHYhMGzXwQmx8YO2H25Oyu1H9ashFbaL1F3of8vDxf00VHisHis0';
 
     const onToken = token => {
-        alert('Payment Successful')
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment Successful')
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error))
+            alert(
+                'There was an issue with your payment. Please use the provided test credit card.'
+            );
+        });
     }
 
     return(
