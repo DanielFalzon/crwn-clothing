3a35bd5d8d149a0cdec59bef104a4fb095412855
Daniel Falzon <daniel.falzon@raketech.com>
2021-08-06 23:47:24 +0200

    Modified PORT env var to fix heroku deployment issue

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV !== 'production') require( 'dotenv' ).config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
